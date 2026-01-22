5da95d36863874852edea263cbfd4d71ce56210c
Akhdan Fadhilah <me@akhdan.dev>
2026-01-22 12:10:35 +0900

    refactor: use explicit fprintf for version output

--- internal/cli/config.go
+++ internal/cli/config.go
@@ -79,7 +79,7 @@ func parseFlags() (*Config, error) {
 	flag.Parse()
 
 	if *showVersion {
-		fmt.Printf("hnkeep version %s, build %s\n", Version, Commit)
+		_, _ = fmt.Fprintf(os.Stdout, "hnkeep version %s, build %s\n", Version, Commit)
 		os.Exit(0)
 	}
 
